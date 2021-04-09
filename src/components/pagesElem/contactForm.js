2b54623e541dd0e1eeac885c12686eb35cc5ec1f
Focus13 <C:\Users\Focus13\AppData\Roaming\The Bat!>
2021-04-09 14:38:56 +0300

    fix: comment netlify email send metod3

--- src/components/pagesElem/contactForm.js
+++ src/components/pagesElem/contactForm.js
@@ -1,141 +1,141 @@
-import React, {useState} from "react"
-import style from'./prise.module.scss'
-import {Link} from 'gatsby'
-import NameStepForm from "./contactFormSteps/NameStepForm"
-import EmailStepForm from "./contactFormSteps/EmailStepForm"
-import PhoneStepForm from "./contactFormSteps/PhoneStepForm"
-import FileStepForm from "./contactFormSteps/FileStepForm"
-import encode from "../utils/encodeEmailContent"
+// import React, {useState} from "react"
+// import style from'./prise.module.scss'
+// import {Link} from 'gatsby'
+// import NameStepForm from "./contactFormSteps/NameStepForm"
+// import EmailStepForm from "./contactFormSteps/EmailStepForm"
+// import PhoneStepForm from "./contactFormSteps/PhoneStepForm"
+// import FileStepForm from "./contactFormSteps/FileStepForm"
+// import encode from "../utils/encodeEmailContent"
 
 
-const ContactForm = ()=>{
-    const [state, setState] = useState({
-        currentStep: 1,
-        name:  '',
-        email: '',
-        phone: '',
-        technicalTask:'',
-    })
-    console.log(state)
+// const ContactForm = ()=>{
+//     const [state, setState] = useState({
+//         currentStep: 1,
+//         name:  '',
+//         email: '',
+//         phone: '',
+//         technicalTask:'',
+//     })
+//     console.log(state)
 
-    const handleChange = event => {
-        const {name, value} = event.target
-        console.log(name, value)
-        console.log(state)
-        setState({
-            ...state,
-          [name]: value
-        })    
-      }
+//     const handleChange = event => {
+//         const {name, value} = event.target
+//         console.log(name, value)
+//         console.log(state)
+//         setState({
+//             ...state,
+//           [name]: value
+//         })    
+//       }
 
-      const handleAttachment = (e) => {
-        setState({ ...state, [e.target.name]: e.target.files[0] })
-      }
-
-
-    const  handleSubmit = event => {
-        event.preventDefault()
-        const form = event.target
-        const { email, name, phone } = state
-        alert(`Your registration detail: \n 
-               Email: ${email} \n 
-               Name: ${name} \n
-               Phone: ${phone}`)
-        // fetch('/', {
-        //   method: 'POST',
-        //   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        //   body: encode({
-        //     'form-name': form.getAttribute('name'),
-        //     ...state,
-        //   }),
-        // })
-        //   .then(() => alert('success')
-        //   .catch((error) => alert(error)))
-    }
+//       const handleAttachment = (e) => {
+//         setState({ ...state, [e.target.name]: e.target.files[0] })
+//       }
 
 
-    const  _next = () => {
-        let currentStep = state.currentStep
-        currentStep = currentStep >= 3? 4: currentStep + 1
-        setState({
-            ...state,
-            currentStep: currentStep,
-          })
-      }
+//     const  handleSubmit = event => {
+//         event.preventDefault()
+//         const form = event.target
+//         const { email, name, phone } = state
+//         alert(`Your registration detail: \n 
+//                Email: ${email} \n 
+//                Name: ${name} \n
+//                Phone: ${phone}`)
+//         // fetch('/', {
+//         //   method: 'POST',
+//         //   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+//         //   body: encode({
+//         //     'form-name': form.getAttribute('name'),
+//         //     ...state,
+//         //   }),
+//         // })
+//         //   .then(() => alert('success')
+//         //   .catch((error) => alert(error)))
+//     }
+
+
+//     const  _next = () => {
+//         let currentStep = state.currentStep
+//         currentStep = currentStep >= 3? 4: currentStep + 1
+//         setState({
+//             ...state,
+//             currentStep: currentStep,
+//           })
+//       }
         
-    const _prev = () => {
-        let currentStep = state.currentStep
-        currentStep = currentStep <= 1? 1: currentStep - 1
-        setState({
-            ...state,
-            currentStep: currentStep,
-        })
-      }
+//     const _prev = () => {
+//         let currentStep = state.currentStep
+//         currentStep = currentStep <= 1? 1: currentStep - 1
+//         setState({
+//             ...state,
+//             currentStep: currentStep,
+//         })
+//       }
 
-      const  previousButton = () => {
-        let currentStep = state.currentStep;
-        if(currentStep !==1){
-          return (
-            <button 
-              className="btn btn-secondary" 
-              type="button" onClick={_prev}>
-            Previous
-            </button>
-          )
-        }
-        return null;
-      }
+//       const  previousButton = () => {
+//         let currentStep = state.currentStep;
+//         if(currentStep !==1){
+//           return (
+//             <button 
+//               className="btn btn-secondary" 
+//               type="button" onClick={_prev}>
+//             Previous
+//             </button>
+//           )
+//         }
+//         return null;
+//       }
       
-      const nextButton = () => {
-        let currentStep = state.currentStep;
-        if(currentStep <4){
-          return (
-            <button 
-              className="btn btn-primary float-right" 
-              type="button" onClick={_next}>
-            Next
-            </button>        
-          )
-        }
-        return null;
-      }
+//       const nextButton = () => {
+//         let currentStep = state.currentStep;
+//         if(currentStep <4){
+//           return (
+//             <button 
+//               className="btn btn-primary float-right" 
+//               type="button" onClick={_next}>
+//             Next
+//             </button>        
+//           )
+//         }
+//         return null;
+//       }
     
-    return(
-        <div className={style.wrapper}>
-             <h1>React Wizard Form 🧙‍♂️</h1>
-            <p>Step {state.currentStep} </p> 
+//     return(
+//         <div className={style.wrapper}>
+//              <h1>React Wizard Form 🧙‍♂️</h1>
+//             <p>Step {state.currentStep} </p> 
 
-            <form onSubmit={handleSubmit}>
-            {/* 
-                render the form steps and pass required props in
-            */}
-                <NameStepForm 
-                currentStep={state.currentStep} 
-                handleChange={handleChange}
-                name={state.name}
-                />
-                <EmailStepForm 
-                currentStep={state.currentStep} 
-                handleChange={handleChange}
-                email={state.email}
-                />
-                <PhoneStepForm 
-                currentStep={state.currentStep} 
-                handleChange={handleChange}
-                phone={state.phone}
-                />
-                <FileStepForm 
-                currentStep={state.currentStep} 
-                handleChange={handleAttachment}
-                technicalTask={state.technicalTask}
-                />
-                {previousButton()}
-                {nextButton()}
-            </form>
-        </div>
-    )
-}
+//             <form onSubmit={handleSubmit}>
+//             {/* 
+//                 render the form steps and pass required props in
+//             */}
+//                 <NameStepForm 
+//                 currentStep={state.currentStep} 
+//                 handleChange={handleChange}
+//                 name={state.name}
+//                 />
+//                 <EmailStepForm 
+//                 currentStep={state.currentStep} 
+//                 handleChange={handleChange}
+//                 email={state.email}
+//                 />
+//                 <PhoneStepForm 
+//                 currentStep={state.currentStep} 
+//                 handleChange={handleChange}
+//                 phone={state.phone}
+//                 />
+//                 <FileStepForm 
+//                 currentStep={state.currentStep} 
+//                 handleChange={handleAttachment}
+//                 technicalTask={state.technicalTask}
+//                 />
+//                 {previousButton()}
+//                 {nextButton()}
+//             </form>
+//         </div>
+//     )
+// }
 
 
 
-export default ContactForm
+// export default ContactForm
