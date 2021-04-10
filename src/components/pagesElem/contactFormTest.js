aaa181c533c201f15d27b3b74e96161fb65aaa22
Дмитрий Леганьков <you@example.com legankov95@gmail.comgit config --global user.name Asindetongit commit -m 'test cv'$ git config --global user.email you@example.com>
2021-04-10 10:23:55 +0300

    f3

--- src/components/pagesElem/contactFormTest.js
+++ src/components/pagesElem/contactFormTest.js
@@ -1,12 +1,6 @@
 
 import React, { useRef, useState, useEffect } from "react"
 import style from "./contactForm.module.scss"
-import NameStepForm from "./contactFormSteps/NameStepForm"
-import EmailStepForm from "./contactFormSteps/EmailStepForm"
-import PhoneStepForm from "./contactFormSteps/PhoneStepForm"
-import FileStepForm from "./contactFormSteps/FileStepForm"
-import classNames from "classnames"
-import SuccessStepForm from "./contactFormSteps/successStepForm"
 
 const ContactForm = () => {
   return (
