c582040fb5b76d099c6abf681f83de951eafba2d
Focus13 <C:\Users\Focus13\AppData\Roaming\The Bat!>
2021-04-14 16:14:27 +0300

    feat: add new style in case

--- src/components/main/KeysBoxs.js
+++ src/components/main/KeysBoxs.js
@@ -1,12 +1,43 @@
 import React from "react"
 import classnames from "classnames"
 import { ButtonGreen, ButtonSmall } from "../buttons"
-import styles from "./KeysBoxs.module.scss"
+import styles from "./testStyle.module.scss"
 import { useStaticQuery, graphql, Link } from "gatsby"
 
 function tagsArea(arr) {
-  const tags = arr.length > 2 ? arr.slpice(0, 2) : arr
-  const tegsArr = tags.map(tag => {})
+  const tags = arr.length > 3 ? arr.slice(0, 3) : arr
+  return tags.map(tag =>{
+    return(
+      <ButtonSmall grey className={classnames(styles.buttonItem, styles.button)} key={tag.id}>
+        <Link to={tag.uri}>
+        {tag.name}
+        </Link>
+    </ButtonSmall>
+    )
+})
+}
+const customGradient ={
+  cG9zdDoyMjMy:{
+    background: `linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 46.35%)`,
+  },
+  cG9zdDoyMjUy:{
+    background: `linear-gradient(180deg, #262626 0%, rgba(0, 0, 0, 0) 46.35%)`,
+  },
+}
+const customBG ={
+  cG9zdDoyMjYw:{
+    backgroundColor: `var(--greenBG-background)`,
+  },
+}
+const customImgSettings ={
+  cG9zdDoyMjYw:{
+    right: `20%`,
+    bottom: `20%`,
+    top: `unset`,
+    left: `unset`,
+    width: `50%`,
+    height: `50%`,
+  },
 }
 
 const KeysBoxs = ({ casesNumber }) => {
@@ -24,7 +55,9 @@ const KeysBoxs = ({ casesNumber }) => {
           uri
           tags {
             nodes {
+              id
               name
+              uri
             }
           }
           featuredImage {
@@ -46,31 +79,46 @@ const KeysBoxs = ({ casesNumber }) => {
       }
     }
   `)
+  console.log(data)
   return (
     <div className={styles.wrapperKeys}>
       {data.allWpPost.nodes.map((item, index) => {
+        console.log(item)
+        let gradientStyle;
+        let bgStyle;
+        let ImgSettings;
+        if(Object.keys(customGradient).includes(item.id)){
+          gradientStyle = customGradient[item.id]
+          console.log(customGradient[item.id])
+        }
+        if(Object.keys(customBG).includes(item.id)){
+          ImgSettings = customImgSettings[item.id];
+          bgStyle = customBG[item.id]
+        }
         const divStyle = {
           backgroundImage: `url(${item.featuredImage.node.sourceUrl})`, //sourceUrl url(https://wp-server.bpm-cloud.by/${item.featuredImage.node.uri})
         }
         return (
+          <div className={styles.caseWrapper} key={item.id} style={bgStyle}>
           <Link
             to={item.uri}
-            key={item.id}
             className={classnames(styles.box, {
               [styles.blockNone]: casesNumber && casesNumber > index,
             })}
           >
-            <div className={styles.caseBgImg} style={divStyle}></div>
-            <div className={styles.title}>Кейсы</div>
-            <div className={classnames(styles.nameKeys)}>{item.title}</div>
-            <ButtonSmall grey className={classnames(styles.button)}>
-              {item.tags.nodes[0].name}
-            </ButtonSmall>
-          </Link>
+            <div className={styles.gradient} style={gradientStyle}></div>
+            <img className={styles.caseBgImg} src={item.featuredImage.node.sourceUrl} style={ImgSettings}/>
+            <div className={classnames(styles.title, styles.text)}>Кейсы</div>
+            <div className={classnames(styles.maneCase, styles.text)}>{item.title}</div>
+            </Link>
+            <div className={styles.tagAreaWrapper}>
+              {tagsArea(item.tags.nodes)}
+            </div>
+          </div>
         )
       })}
 
-      <div className={styles.boxLast}>
+      <div className={classnames(styles.caseWrapper,styles.boxLast) }>
         <div className={classnames(styles.titleLast)}>Все кейсы</div>
         <ButtonGreen className={classnames(styles.button, styles.lastButton)}>
           смотреть все
