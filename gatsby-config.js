d91dc7020deaac07228fc67f99b3649a4cd7b1c1
Focus13 <C:\Users\Focus13\AppData\Roaming\The Bat!>
2021-04-16 11:07:38 +0300

    feat:add User-agent: *

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-sitemap`,
+    'gatsby-plugin-robots-txt',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
